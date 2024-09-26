7b7ffe297d13a19810e6c617dd7c54a2c5105105
sharmajikiladki <natanshi04@gmail.com>
2024-09-27 02:20:45 +0530

    Add: test case - version 2

--- tests/test.js
+++ tests/test.js
@@ -13,4 +13,17 @@ describe("Library Management System", () => {
     expect(temp1.length).toBe(1);
     expect(temp1[0].title).toBe("Textbook1");
   });
+
+  test("Borrowing", () => {
+    library.addBook("1", "Textbook1", "reva buche", 2020);
+    library.borrow("1");
+    const temp2 = library.available();
+    expect(availableBooks.length).toBe(0);
+  });
+
+  test("Unavailable book error", () => {
+    library.addBook("1", "Textbook1", "reva buche", 2020);
+    library.borrow("1");
+    expect(() => library.borrow("1")).toThrow("Book already borrowed");
+  });
 });
